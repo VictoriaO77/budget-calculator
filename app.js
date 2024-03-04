4c780071294276c305e8785478bd8a46ea7d27b5
VictoriaO77 <143848611+VictoriaO77@users.noreply.github.com>
2024-03-04 14:36:44 -0800

    Noah
    Started JavaScript

---
+++ app.js
@@ -0,0 +1,16 @@
+let jobList = new Map();
+
+jobList.set(`Chef`, 69382.00)
+jobList.set(`Chemist Civil Eng.`, 58808.00)
+
+
+document.addEventListener(`DOMContentLoaded`, () =>{
+
+    let elementById = function(id) {return document.getElementById(id)}
+
+    let addListener = function(id,event,code) {elementById(id).addEventListener(event, (eventData) => {code(eventData)})}
+
+    //THIS can be rovmoved it was to try add save time typing it all by hand
+    let tester = elementById(`placeholder`)
+
+})
